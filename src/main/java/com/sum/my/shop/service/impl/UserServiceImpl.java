3a4112dbf6d13bf6fb81b966e652df6590aa9766
swj369451 <swj369451@163.com>
2020-02-17 22:05:44 +0800

    spring初始化xml的BUG地方

--- src/main/java/com/sum/my/shop/service/impl/UserServiceImpl.java
+++ src/main/java/com/sum/my/shop/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.sum.my.shop.service.UserService;
 
 public class UserServiceImpl implements UserService {
 
+
 //    如果将这俩句放在属性的位置会报错误
 //    SpringContext springContext = new SpringContext();
 //    UserDao userDao = (UserDao) springContext.getBean("userDao");
